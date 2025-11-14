36acee34e202c8d80ab9219d0d6987d4e203c8f0
Tejesh Naidu <151620278+TejeshNaiduKona@users.noreply.github.com>
2025-11-14 07:10:59 +0530

    Add newline at end of start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 # Run Streamlit on port 8501 and FastAPI on port 8000
 streamlit run streamlit_app.py &  # runs Streamlit in background
 uvicorn api_server:app --host 0.0.0.0 --port 8000 --reload
+ 
